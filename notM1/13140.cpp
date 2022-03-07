16fd714b6609ced4d0bdd57e267cb0afc0bba52d
이재호 <iamjaeholee@cashwalk.io>
2022-03-07 14:26:26 +0900

    hellow world gp

--- notM1/13140.cpp
+++ notM1/13140.cpp
@@ -7,10 +7,10 @@ int main(){
 
   // 전설의 7중 포문
   for(int h=1; h<10; h++) {
-    for(int w=1; w<10; w++) {
-      for(int e=0; e<10; e++) {
-        for(int l=0; l<10; l++) {
-          for(int o=0; o<10; o++) {
+    for(int e=0; e<10; e++) {
+      for(int l=0; l<10; l++) {
+        for(int o=0; o<10; o++) {
+          for(int w=1; w<10; w++) {
             for(int r=0; r<10; r++) {
               for(int d=0; d<10; d++) {
                 if(h == e || 
@@ -40,7 +40,9 @@ int main(){
                 unsigned int result = hello+world;
                 if(result == N) {
                   check=1;
-                  std::cout << "  " << hello << std::endl << "+ " << world << std::endl << "-------" << std::endl << result;
+                  std::cout << "  " << hello << std::endl << "+ " << world << std::endl << "-------" << std::endl;
+                  std::cout << ((result/100000 == 1) ? " " : "  ");
+                  std::cout << N;
                   return 0;
                 }
               }
