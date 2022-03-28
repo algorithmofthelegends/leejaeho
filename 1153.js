b462391890501e945d73218b582ee26e56ae8e25
이재호 <iamjaeholee@cashwalk.io>
2022-03-28 15:18:15 +0900

    에라토스테네스의 체, 완전탐색

--- 1153.js
+++ 1153.js
@@ -1,11 +1,48 @@
-const numbers = [
-  ...Array.from(Array(1000000).keys()).filter((v) => v !== 0 && v !== 1),
-];
+const localInput = `
+1000000
+`;
+
+const input = +(
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    : localInput
+).trim();
+
+const numbers = [...Array.from(Array(input + 1).keys()).fill(true)];
 
 const primeNumber = [];
 
-numbers.forEach((v, i) => {
-  if (primeNumber.every((p) => v % p !== 0)) primeNumber.push(v);
+numbers.map((v, i) => {
+  if (i === 0 || i === 1) return false;
+
+  if (v) {
+    if (i * i <= input) {
+      let pow = 1;
+      let n = i * pow;
+      while ((n = i * pow) <= input) {
+        numbers[n] = false;
+        pow++;
+      }
+    }
+
+    primeNumber.push(i);
+  }
+
+  return v;
 });
 
-console.log(primeNumber);
+const getPrimes = () => {
+  primeNumber.forEach((a) => {
+    primeNumber.forEach((b) => {
+      primeNumber.forEach((c) => {
+        primeNumber.forEach((d) => {
+          if (a + b + c + d === input) return [a, b, c, d];
+        });
+      });
+    });
+  });
+};
+
+const result = getPrimes();
+
+console.log(result ? result.join(" ") : -1);
