1c470fbab066995e502dba6661892cd52d12eda0
이재호 <leejaeho@ijaehos-MacBook-Air.local>
2022-03-28 19:14:00 +0900

    네자리 소수 순열 적용

--- 1153.js
+++ 1153.js
@@ -1,38 +1,6 @@
 const localInput = `
-38
+6
 `;
-
-const input = +(
-  process.platform === "linux"
-    ? require("fs").readFileSync("/dev/stdin").toString()
-    : localInput
-).trim();
-
-const numbers = [...Array.from(Array(input + 1).keys()).fill(true)];
-
-const primeNumber = [];
-
-numbers.map((v, i) => {
-  if (i === 0 || i === 1) return false;
-
-  if (v) {
-    if (i * i <= input) {
-      let pow = 1;
-      let n = i * pow;
-      while ((n = i * pow) <= input) {
-        numbers[n] = false;
-        pow++;
-      }
-    }
-
-    primeNumber.push(i);
-    primeNumber.push(i);
-    primeNumber.push(i);
-    primeNumber.push(i);
-  }
-
-  return v;
-});
 
 const nextPermutation = (l) => {
   let firstDecreasing = -1;
@@ -66,26 +34,49 @@ const nextPermutation = (l) => {
   }
 };
 
-let pem = new Array(primeNumber.length - 4).fill(0);
-pem.push(1);
-pem.push(1);
-pem.push(1);
-pem.push(1);
+const input = +(
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    : localInput
+).trim();
+
+const numbers = [...Array.from(Array(input + 1).keys()).fill(true)];
+
+let primeNumber = [];
+
+numbers.map((v, i) => {
+  if (i === 0 || i === 1) return false;
+
+  if (v) {
+    if (i * i <= input) {
+      let pow = 1;
+      let n = i * pow;
+      while ((n = i * pow) <= input) {
+        numbers[n] = false;
+        pow++;
+      }
+    }
+
+    primeNumber.push(i);
+    primeNumber.push(i);
+    primeNumber.push(i);
+    primeNumber.push(i);
+  }
+
+  return v;
+});
+
+
 
 let result;
 while (true) {
-  let t = [];
-  pem.forEach((v, i) => {
-    if (v === 1) t.push(primeNumber[i]);
-  });
-
-  if (t[0] + t[1] + t[2] + t[3] === input) {
-    result = t;
+  if (primeNumber[0] + primeNumber[1] + primeNumber[2] + primeNumber[3] === input) {
+    result = primeNumber.slice(0, 4);
     break;
   }
 
-  const [nextL, check] = nextPermutation(pem);
-  if (check) pem = nextL;
+  const [nextPrimeNumber, check] = nextPermutation(primeNumber);
+  if (check) primeNumber = nextPrimeNumber;
   else break;
 }
 
