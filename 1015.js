510d2b77e49a623c417d4d6ea0909e11b18d622f
이재호 <iamjaeholee@cashwalk.io>
2022-03-30 15:38:15 +0900

    비내림차순

--- 1015.js
+++ 1015.js
@@ -11,20 +11,17 @@ const input = (
   .trim()
   .split("\n");
 
-const N = parseInt(input.shift());
-if (N !== 0) {
-  const A = input
-    .shift()
-    .trim()
-    .split(/\s+/)
-    .map((v) => +v);
-  const B = Array.from(A);
-  B.sort();
+const N = input.shift();
+const A = input
+  .shift()
+  // .trim()
+  .split(/\s+/)
+  .map((v) => +v);
+const B = [...A];
+B.sort((a, b) => a - b);
 
-  const history = new Array(N).fill(0);
-  A.forEach((v, i) => {
-    const p = B.indexOf(v, history[v]);
-    history[v] = p + 1;
-    process.stdout.write(p + (i === A.length - 1 ? "" : " "));
-  });
-}
+A.forEach((v, i) => {
+  const p = B.indexOf(v);
+  B[p] = -1;
+  process.stdout.write(p + (i === A.length - 1 ? "" : " "));
+});
