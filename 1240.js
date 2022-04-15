094f0bb22689d959f5fcea6c9b00eeb06f400d8e
이재호 <iamjaeholee@cashwalk.io>
2022-04-15 12:24:28 +0900

    두 개의 노드

--- 1240.js
+++ 1240.js
@@ -18,6 +18,7 @@ const input = (
 // search logic
 const bfs = (start, dest, nodes) => {
   const n = nodes.length;
+  // [0,0,0,0,0,0]
   const footprint = new Array(n + 1).fill(0, 0, n + 1);
 
   const q = [];
@@ -25,6 +26,15 @@ const bfs = (start, dest, nodes) => {
   footprint[start] = 1;
 
   while (q.length) {
+    /**
+     * 1 , 0
+     * 2 , 2
+     *
+     * 3 , 0
+     * 4 , 2
+     * 1 , 5
+     * 2 , 7
+     */
     const { node, count } = q.shift();
     // if get to destination stop
     if (node === dest) {
@@ -47,6 +57,12 @@ let [N, M] = input
   .split(/\s+/)
   .map((v) => +v);
 
+/**
+   *[[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1]]
+    [[-1,-1,-1,-1,-1],[-1,-1,2,-1,-1],[-1,2,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1]]
+    [[-1,-1,-1,-1,-1],[-1,-1,2,-1,-1],[-1,2,-1,-1,-1],[-1,-1,-1,-1,2],[-1,-1,-1,2,-1]]
+    [[-1,-1,-1,-1,-1],[-1,-1,2,-1,3],[-1,2,-1,-1,-1],[-1,-1,-1,-1,2],[-1,3,-1,2,-1]] 
+   */
 const nodes = new Array(N + 1).fill(0, 0, N + 1).map((v) => {
   return new Array(N + 1).fill(-1, 0, N + 1);
 });
