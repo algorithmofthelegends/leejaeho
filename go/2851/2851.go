b23e810af9c3c2297a4635a20dfbb66b55327a08
이재호 <iamjaeholee@cashwalk.io>
2022-05-26 16:01:09 +0900

    주석 추가

--- go/2851/2851.go
+++ go/2851/2851.go
@@ -15,6 +15,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -22,11 +24,23 @@ func main() {
 
 	result := uint32(0)
 
+	f1, err := os.Create("2851.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f1.Close()
+
+	/*
+		1 3 6 11 19 32 53 87 142 231
+	*/
 	for i := range mushroom {
 		fmt.Scanf("%d", &mushroom[i])
 		if i != 0 {
 			mushroom[i] += mushroom[i-1]
 		}
+
+		fmt.Fprintf(f1, strconv.Itoa(int(mushroom[i]))+" ")
 
 		if math.Abs(float64(100)-float64(result)) >= math.Abs(float64(100)-float64(mushroom[i])) && result < mushroom[i] {
 			result = mushroom[i]
